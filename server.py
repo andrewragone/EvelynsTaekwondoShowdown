c23141d1140c9c5769af43e183e1f23cbe0e6a5d
andrew ragone <andrew.ragone@gmail.com>
2025-04-13 01:30:39 -0400

    added walking animation.

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
+import os
 
 PORT = 8000
 
 class Handler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory='.', **kwargs)
+    
+    def guess_type(self, path):
+        # Override MIME type guessing for common image types
+        ext = os.path.splitext(path)[1]
+        if ext.lower() in ('.jpg', '.jpeg'):
+            return 'image/jpeg'
+        elif ext.lower() == '.png':
+            return 'image/png'
+        return super().guess_type(path)
 
 with HTTPServer(('localhost', PORT), Handler) as httpd:
     print(f'Serving on http://localhost:{PORT}')
